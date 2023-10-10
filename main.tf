6765f2a5db1c7fb809929e1c494f8f557675fd3e
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-10-10 17:42:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider  "aws" {
    region = var.aws_region
 }
 
-resource "aws_instance" "testinstance"{
-#   count = 3
+resource "aws_instance" "terr-instance"{
+#   count = 1
    ami = var.ami
    instance_type = var.instance_type
    key_name = var.key
